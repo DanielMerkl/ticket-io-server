aeed4b0f0a9fdb9a3bc7db5ac468bdda0a6ce325
Daniel Merkl <daniel.merkl.dm@gmail.com>
2022-03-31 18:43:59 +0200

    init separate tickets resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { EventsModule } from './events/events.module';
+import { TicketsModule } from './tickets/tickets.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), EventsModule],
+  imports: [ConfigModule.forRoot(), EventsModule, TicketsModule],
   controllers: [AppController],
   providers: [AppService],
 })
