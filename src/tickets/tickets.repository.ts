35d8c7cf5eb890afd2c059559c56592d04047560
Daniel Merkl <daniel.merkl.dm@gmail.com>
2022-03-31 19:18:25 +0200

    implement tickets repository

---
+++ src/tickets/tickets.repository.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { Ticket } from './entities/ticket.entity';
+
+@Injectable()
+export class TicketsRepository {
+  private tickets: Map<string, Ticket> = new Map<string, Ticket>();
+
+  public findAll(): Ticket[] {
+    return [...this.tickets.values()];
+  }
+
+  public findById(id: string): Ticket {
+    return this.tickets.get(id);
+  }
+
+  public save(ticket: Ticket): void {
+    this.tickets.set(ticket.eventId + ticket.barcode, ticket);
+  }
+
+  public delete(id: string): void {
+    this.tickets.delete(id);
+  }
+}
